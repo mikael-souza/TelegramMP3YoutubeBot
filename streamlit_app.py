f9ca4154ac1b9c52d7ef689f4dd13a01e3567c6c
mikael-souza <contato@mikaelsouza.com.br>
2022-02-10 18:24:09 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,39 @@
-streamlit_app.py
+from bot import telegram_chatbot
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.debug = True
+
+bot = telegram_chatbot("config.cfg")
+
+@app.route('/webhook', methods=['POST'])
+def make_reply(msg):
+    reply = None
+    if msg is not None:
+#        reply = msg
+        if "youtube.com/watch?v=" in msg or "youtu.be/" in msg:
+            reply = msg
+        else:
+            reply = None
+    return reply
+
+update_id = None
+while True:
+    updates = bot.get_updates(offset=update_id)
+    updates = updates["result"]
+    if updates:
+        for item in updates:
+            update_id = item["update_id"]
+            try:
+                message = str(item["message"]["text"])
+            except:
+                message = None
+            first_name = item["message"]["from"]["first_name"]
+            from_ = item["message"]["from"]["id"]
+            reply = make_reply(message)            
+#            bot.send_message(reply, from_)
+            if reply is not None:
+                bot.send_audio(reply, from_)
+            else:
+                reply = "Please, send a valid video url."
+                bot.send_message(reply, from_)
